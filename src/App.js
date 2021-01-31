0f8175576ee86a521f065c0ca09e04b890878a18
SaladinDacic <saladindacic@gmail.com>
2021-01-31 21:47:00 +0100

    Fix Palette Route

--- src/App.js
+++ src/App.js
@@ -3,18 +3,36 @@ import seedColors from "./seedColors";
 import {Route, Switch} from "react-router-dom";
 import {generatePalette} from "./colorHelpers";
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  console.log(generatePalette(seedColors[4]))
-  return (
-    <Switch>
-      <Route exact path="/" render={()=><h1>Palette list goes here!!</h1>}/>
-      <Route exact path="/palette/:id" render={()=><h1>Individual palette</h1>}/>
-    </Switch>
- /*    <div >
-      <Palette palette={generatePalette(seedColors[4])}/>
-    </div> */
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.findPalette = this.findPalette.bind(this);
+  }
+  
+  findPalette(id){
+    return seedColors.find((palette)=>{
+      return palette.id === id;
+    })
+  }
+  render(){
+    // console.log(generatePalette(seedColors[4]))
+    return (
+      <Switch>
+        <Route exact path="/" render={()=><h1>Palette list goes here!!</h1>}/>
+        <Route 
+          exact 
+          path="/palette/:id" 
+          render={(routeProps)=><Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>}
+        />
+      </Switch>
+    /*    <div >
+        <Palette palette={generatePalette(seedColors[4])}/>
+      </div> */
+    )
+  }
 }
+
 
 export default App;
