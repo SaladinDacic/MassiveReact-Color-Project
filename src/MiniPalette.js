f4ccc1565d470fc9fb15f04cabf5250b5f580cfa
SaladinDacic <saladindacic@gmail.com>
2021-02-05 19:22:21 +0100

    Finish styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,47 +1,64 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
-import PaletteList from './PaletteList';
+// import PaletteList from './PaletteList';
 
 const styles = {
 	root: {
-		backgroundColor: 'white',
-		border: '1px solid black',
-		borderRadius: '5px',
-		padding: '0.5rem',
-		position: 'relative',
-		overflow: 'hidden',
-		'&:hover': {
-			cursor: 'pointer'
+		backgroundColor: "white",
+		border: "1px solid black",
+		borderRadius: "5px",
+		padding: "0.5rem",
+		position: "relative",
+		overflow: "hidden",
+		"&:hover": {
+		  cursor: "pointer"
 		}
-	},
-	colors: {
-		backgroundColor: 'grey'
-	},
-	title: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		marging: '0',
-		color: 'black',
-		paddingTop: '0.5rem',
-		fontSize: '1rem',
-		position: 'relative'
-	},
-	"title__emoji": {
-		marginLeft: '0.5rem',
-		fontSize: '1.5rem'
-	}
+	  },
+	  colors: {
+		backgroundColor: "#dae1e4",
+		height: "150px",
+		width: "100%",
+		borderRadius: "5px",
+		overflow: "hidden"
+	  },
+	  title: {
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "center",
+		margin: "0",
+		color: "black",
+		paddingTop: "0.5rem",
+		fontSize: "1rem",
+		position: "relative"
+	  },
+	  emoji: {
+		marginLeft: "0.5rem",
+		fontSize: "1.5rem"
+	  },
+	  miniColor: {
+		height: "25%",
+		width: "20%",
+		display: "inline-block",
+		margin: "0 auto",
+		position: "relative",
+		marginBottom: "-3.5px"
+	  }
 };
 
 function MiniPalette(props) {
-	const { classes, paletteName, emoji } = props;
+	const { classes, paletteName, emoji, colors } = props;
+	const miniColorBoxes = colors.map((color) => {
+		return <div className={classes.miniColor} style={{ backgroundColor: color.color }} key={color.name}/>;
+	});
 	console.log(classes, paletteName, emoji);
 	return (
 		<div className={classes.root}>
-			<div className={classes.colors} />
-			<h5 className={classes.title}>
-				{paletteName} <span className={classes.emoji}>{emoji}</span>
-			</h5>
+			<div className={classes.container}>
+				<div className={classes.colors}>{miniColorBoxes}</div>
+				<h5 className={classes.title}>
+					{paletteName} <span className={classes.emoji}>{emoji}</span>
+				</h5>
+			</div>
 		</div>
 	);
 }
